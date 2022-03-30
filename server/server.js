f68fa886528b016be3a8471e92ec62e68e001fe3
Jacob Morse <jacobsmorse@gmail.com>
2022-03-30 09:02:16 -0600

    updated get request for heroku in server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, ".../public")))
+// app.use(express.static(path.join(__dirname, ".../public")))
+
+app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public"))
+    })
 
 
 const reviews = [
